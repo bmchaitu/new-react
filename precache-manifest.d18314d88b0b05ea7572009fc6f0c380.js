4f5ba9f1af3b537c89c9df1a497676d884fbd9dd
iamfine420 <budagavichaitu@gmail.com>
2019-08-12 15:18:48 +0530

    Updates

---
+++ precache-manifest.d18314d88b0b05ea7572009fc6f0c380.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87f94cd26ab0872f40e87a83b7f3b0bb",
+    "url": "/new-react/index.html"
+  },
+  {
+    "revision": "0f8032ac87f0081c1a27",
+    "url": "/new-react/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "078b13cef65590a7b16a",
+    "url": "/new-react/static/js/2.75e1d33b.chunk.js"
+  },
+  {
+    "revision": "0f8032ac87f0081c1a27",
+    "url": "/new-react/static/js/main.4ee6fc29.chunk.js"
+  },
+  {
+    "revision": "450272ca0c639c27d2ec",
+    "url": "/new-react/static/js/runtime~main.8fe0527a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/new-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
